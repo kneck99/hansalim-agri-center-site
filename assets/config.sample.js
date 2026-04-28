d2c002cb6941dee57bf72e311c0e2b56823590d5
kneck99 <kneck.n.o.d@gmail.com>
2026-04-28 10:59:56 +0900

    Update config.sample.js

--- assets/config.sample.js
+++ assets/config.sample.js
@@ -1,6 +1,6 @@
 window.HS_CONFIG = {
   siteTitle: '한살림 농업살림센터',
-  reserveUrl: 'https://script.google.com/macros/s/AKfycbydXDPlT6wckhE3BBFL_b3XXLVoxzrksqcyPNe0VC8MIl-dTrVUwE6Ot0yPPG-RqtLJzw/exec',
+  reserveUrl: 'https://script.google.com/macros/s/AKfycbzYNboXx69-QRESKrRmU71cVFrp4jCS98slWTq0HQ3SOZ8tmQe6tXRhxpMoHUNoxW_v-A/exec',
   calendarEmbedUrl: 'https://calendar.google.com/calendar/embed?src=1bdf95f3ac12bca438d77a43df12225b04ae9f5d2d1f5c72677d17dc04a35c11%40group.calendar.google.com&ctz=Asia%2FSeoul',
   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3202.10718715604!2d127.54589971163912!3d36.62380647750713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356527f1d5bc0b47%3A0x6425863b2f948846!2z7ZWc7IK066a864aN7JeF7IK066a87IS87YSw!5e0!3m2!1sko!2skr!4v1777279080614!5m2!1sko!2skr',
   business: {
